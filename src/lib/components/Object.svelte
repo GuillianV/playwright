5c4e381dddfd89c189a3c32c83793a51d62fd6d7
GuillianV <guillianvibert90@gmail.com>
2023-06-06 14:00:17 +0200

    feat: add pre

---
+++ src/lib/components/Object.svelte
@@ -0,0 +1,36 @@
+<script>
+    export let object;
+</script>
+
+<div>
+    <pre>
+        
+        <code>{JSON.stringify(object, null, 3)}</code>
+    </pre>
+</div>
+
+
+<style >
+
+    div {
+
+        display: flex;
+        
+    }
+
+    pre{
+        border-radius: 16px;
+        background-color: #dadada;
+        padding: 16px;
+        margin: 16px;
+        font-family: 'comfortaa';
+        letter-spacing: 1px;
+        border: 1px solid #eefefe;
+        box-shadow:  2px 2px 2px #000, inset 2px 2px 2px  #dadada;
+    }
+    code{
+        
+        line-height: 3vh;
+    }
+
+</style>
--- src/routes/(admin)/admin/home/+page.svelte
+++ src/routes/(admin)/admin/home/+page.svelte
@@ -1,12 +1,12 @@
 <script>
     import H1 from '$lib/components/H1.svelte';
+    import Object from '$lib/components/Object.svelte';
     import { page } from '$app/stores'
 
     console.log($page.data.user)
 </script>
 
+<Object object={$page.data.user} />
 You are logged in Admin
 
-<pre>
-  {$page.data.user}
-</pre>
+
