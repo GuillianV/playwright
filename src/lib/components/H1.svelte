89f1a8a0df4481b9623875af76a99fdd7875feac
GuillianV <guillianvibert90@gmail.com>
2023-06-06 13:38:17 +0200

    feat: add auth

---
+++ src/lib/components/H1.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let innerText = innerText.length <= 0 ? "Undefined" : innerText
+
+</script>
+<h1>{innerText}</h1>
+
+<style lang="scss">
+
+    h1{
+        color:#FFFBFC;
+        font-family: "Bebas Neue";
+        font-size: 3rem;
+        border-bottom: 8px solid #D30C7B;
+        width: fit-content;
+        line-height: 2.8rem;
+        letter-spacing: 0.8rem;
+        margin: 16px;
+    }
+</style>
---
+++ src/routes/(admin)/admin/home/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import H1 from '$lib/components/H1.svelte';
+    import { page } from '$app/stores'
+
+    console.log($page.data.user)
+</script>
+
+You are logged in Admin
+
+<pre>
+  {$page.data.user}
+</pre>
