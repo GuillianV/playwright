486dfe51869d79d142f5d946ac7c90fb0412ad3c
GuillianV <guillianvibert90@gmail.com>
2023-06-22 10:32:38 +0200

    fat: api

--- src/routes/(admin)/admin/register/+page.svelte
+++ src/routes/(admin)/admin/register/+page.svelte
@@ -10,31 +10,19 @@
 		<form action="?/register" method="POST">
 			<div>
 				<label for="username">Nom d'utilisateur</label><br />
-<<<<<<< HEAD
 				<input aria-label="username" id="username" name="username" type="text" required />
-=======
-				<input id="username" name="username" type="text" required />
->>>>>>> f9b3732ccd2c928279a9d0b91d8f8fcaed44c5dc
 			</div>
 			<br />
 			<div>
 				<label for="password">Mot de passe</label><br />
-<<<<<<< HEAD
 				<input aria-label="password" id="password" name="password" type="password" required />
-=======
-				<input id="password" name="password" type="password" required />
->>>>>>> f9b3732ccd2c928279a9d0b91d8f8fcaed44c5dc
 			</div>
 
 			{#if form?.user}
 				<p class="error">Utilisateur déjà utilisé.</p>
 			{/if}
 
-<<<<<<< HEAD
 			<button aria-label="submit" class="submitButton" name="Register" type="submit">S'enregistrer</button>
-=======
-			<button class="submitButton" name="Register" type="submit">S'enregistrer</button>
->>>>>>> f9b3732ccd2c928279a9d0b91d8f8fcaed44c5dc
 		</form>
 	</div>
 </section>
